bf1d93f7acbd4f390ea06eb576ec3e17d9f0f384
zxx <812603872@qq.com>
2019-06-13 20:47:39 +0800

    基础

--- server/schedule.js
+++ server/schedule.js
@@ -1,20 +1,19 @@
-class Schedule{
-    constructor(fromDate, toDate, triggerFunc, content){
-        const fromDate = Date.parse(fromDate);
-        const toDate = Date.parse(toDate);
-        if(fromDate instanceof Date && toDate instanceof Date){
-            // 计划的结束时间必须大于起始时间5秒
-            if(toDate.getTime() - fromDate.getTime() > 5){
-                const wrapTrigger = () => {
-                    if(Date.now() >= fromDate.getTime() && Date.now() <= toDate.getTime()){
-                        triggerFunc();
-                    }
-                this.evtId = setInterval(wrapTrigger, 1000);
-                }
-
-            } else {
-                throw 'date wrong';
-            }
-        }
+class Schedule {
+  constructor(fromDateStr, toDateStr, triggerFunc, content) {
+    const fromDate = Date.parse(fromDateStr);
+    const toDate = Date.parse(toDateStr);
+    if (fromDate instanceof Date && toDate instanceof Date) {
+      // 计划的结束时间必须大于起始时间5秒
+      if (toDate.getTime() - fromDate.getTime() > 5) {
+        const wrapTrigger = () => {
+          if (Date.now() >= fromDate.getTime() && Date.now() <= toDate.getTime()) {
+            triggerFunc();
+          }
+          this.evtId = setInterval(wrapTrigger, 1000);
+        };
+      } else {
+        throw 'data wr1on1g';
+      }
     }
+  }
 }
